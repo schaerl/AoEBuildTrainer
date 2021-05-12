0e7dbf7b4080d267147483938a2df467c6879c0f
Lukas Schär <lukas@schaer-online.ch>
2021-05-12 14:52:33 +0200

    Added settings menu and transition to add frag

--- app/src/main/java/ch/feosoft/aoebuildtrainer/BuildOrderListFragment.kt
+++ app/src/main/java/ch/feosoft/aoebuildtrainer/BuildOrderListFragment.kt
@@ -1,20 +1,24 @@
 package ch.feosoft.aoebuildtrainer
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ch.feosoft.aoebuildtrainer.adapter.BuildOrderAdapter
 import ch.feosoft.aoebuildtrainer.data.DataSource
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class BuildOrderListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -28,5 +32,15 @@ class BuildOrderListFragment : Fragment() {
         val recycler = view.findViewById<RecyclerView>(R.id.build_order_recycler_view)
         recycler.layoutManager = LinearLayoutManager(requireContext())
         recycler.adapter = BuildOrderAdapter(DataSource.getData())
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { v ->
+            v.findNavController().navigate(BuildOrderListFragmentDirections.actionListFragmentToAddFragment())
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
     }
 }
