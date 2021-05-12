0d827404592f118d86fd3b858a7f290423e97226
Lukas Schär <lukas@schaer-online.ch>
2021-05-12 14:21:29 +0200

    Cleanup of activity and added second fragment
    Main activity is now only a holder for the fragments, nothing more.
    The FAB is now part of the list fragment.
    Adder fragment placeholder inserted
    Added safeArgs plugin

--- app/src/main/java/ch/feosoft/aoebuildtrainer/BuildOrderListFragment.kt
+++ app/src/main/java/ch/feosoft/aoebuildtrainer/BuildOrderListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ch.feosoft.aoebuildtrainer.adapter.BuildOrderAdapter
@@ -16,16 +17,10 @@ import ch.feosoft.aoebuildtrainer.data.DataSource
 class BuildOrderListFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {/* Inflate the layout for this fragment*/
-
-
-        val view = inflater.inflate(R.layout.fragment_build_order, container, false)
-
-
-        return view
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        /* Inflate the layout for this fragment*/
+        return inflater.inflate(R.layout.fragment_build_order_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
